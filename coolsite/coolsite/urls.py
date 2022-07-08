6b6614ae02342c5a98d49caa31c3f5853764fdbe
kodzyz <kodzyzdok@gmail.com>
2022-07-08 11:42:22 +0300

    базовые представления/маршрутизация

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from women.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('cats/', categories),
 ]
