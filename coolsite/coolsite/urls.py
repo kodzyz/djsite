bc07cf7d6313b3b989d7dd7be9d030219ca89551
kodzyz <kodzyzdok@gmail.com>
2022-07-08 12:08:52 +0300

    принцип "разделяй и властвуй" представления/маршрутизация: include()

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from women.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('cats/', categories),
+
+    #women
+    path('women/', include('women.urls')),
+
 ]
