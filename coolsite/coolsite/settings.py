de62c3aae410346a154435d33ab772574522134b
kodzyz <kodzyzdok@gmail.com>
2022-07-14 15:58:08 +0300

    Подключение статических файлов CSS

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -113,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  #префикс URL-адреса для статических файлов, который будет добавляться к URL статических файлов
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  #путь к общей статической папке, используемой реальным веб-сервером
+STATICFILES_DIRS = []  #список дополнительных (нестандартных) путей к статическим файлам
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
