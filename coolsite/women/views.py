995db65000bbca0308a5d031b7ff2d680c880a7d
ko_dzy <kodzyzdok@gmail.com>
2022-07-16 00:21:16 +0300

    URL-адреса в шаблонах - метод 1  {% url 'имя маршрута'[доп параметры] %}

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,30 +2,43 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить статью", 'url_name': 'add_page'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'}
+]
 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def categories(request, cat):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{cat}</p>')
+def addpage(request):
+    return HttpResponse("Добавление стратьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home', permanent=True)
-
-    return HttpResponse(f'<h1>Архив по годам</h1>{year}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
