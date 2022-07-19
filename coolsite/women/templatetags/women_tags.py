2d4acfb2ee98c7f30857738e0d9ce1c6bdf4cc6e
ko_dzy <kodzyzdok@gmail.com>
2022-07-20 00:29:17 +0300

    тема11: пользовательские теги шаблонов 5.1) передача параметров простому тегу

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -1,17 +1,18 @@
 from django import template
 from women.models import *
 
-# регистрация собств. шаблонных тегов
 register = template.Library()
 
 
 @register.simple_tag(name='getcats')
-def get_categories():
-    return Category.objects.all()
+def get_categories(filter=None):  # именнованный параметр
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
 
 
-# включающий тег - формир. фрагмента HTML страницы
 @register.inclusion_tag('women/list_categories.html')
 def show_categories():
     cats = Category.objects.all()
-    return {"cats": cats}  # "cats" будет передаваться шаблону 'women/list_categories.html'
+    return {"cats": cats}
