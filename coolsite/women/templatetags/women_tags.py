91df24200de982e2a0006113dda3c3806aae0b6c
ko_dzy <kodzyzdok@gmail.com>
2022-07-20 00:19:12 +0300

    тема11: пользовательские теги шаблонов 4) включающий тег

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -8,3 +8,10 @@ register = template.Library()
 @register.simple_tag(name='getcats')
 def get_categories():
     return Category.objects.all()
+
+
+# включающий тег - формир. фрагмента HTML страницы
+@register.inclusion_tag('women/list_categories.html')
+def show_categories():
+    cats = Category.objects.all()
+    return {"cats": cats}  # "cats" будет передаваться шаблону 'women/list_categories.html'
