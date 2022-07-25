618b19a934394f530df5685d96f7a68fca38115d
ko_dzy <kodzyzdok@gmail.com>
2022-07-26 00:28:45 +0300

    13 Использование форм, не связанных с моделями 3) внешний вид формы и атрибуты объекта form внутри шаблона

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -3,8 +3,10 @@ from .models import *
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label='Заголовок')  # атрибут label, который и позволяет задавать свои имена
+    slug = forms.SlugField(max_length=255, label='URL')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент',
+                              required=False)  # сделаем поле content необязательным
+    is_published = forms.BooleanField(label='Публикация', initial=True)  # поле is_published с установленной галочкой
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории',
+                                 empty_label='Категория не выбрана')  # фраза вместо черточек
