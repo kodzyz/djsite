8476fbe148eb10ccf71e41dd8c8e3f8ea4a40f4f
ko_dzy <kodzyzdok@gmail.com>
2022-07-26 00:42:06 +0300

    13 Использование форм, не связанных с моделями 4) цикл в шаблоне и стили оформлений в классе формы

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -3,10 +3,10 @@ from .models import *
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок')  # атрибут label, который и позволяет задавать свои имена
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))  # стили оформлений можно прописать непосредственно в классе формы
     slug = forms.SlugField(max_length=255, label='URL')
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент',
-                              required=False)  # сделаем поле content необязательным
-    is_published = forms.BooleanField(label='Публикация', initial=True)  # поле is_published с установленной галочкой
+                              required=False)
+    is_published = forms.BooleanField(label='Публикация', initial=True)
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории',
-                                 empty_label='Категория не выбрана')  # фраза вместо черточек
+                                 empty_label='Категория не выбрана')
